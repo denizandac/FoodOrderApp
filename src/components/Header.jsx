39c9100f737a55f13a6ddeb9c2f668bb381e3b41
Deniz <denizandac33@gmail.com>
2024-02-22 04:23:21 +0300

    Cart Connection
    -connected the buttons and the cart modal

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -5,12 +5,20 @@ import logo from "../assets/logo.jpg";
 import Button from "./UI/Button.jsx";
 //Context
 import CartContext from "../store/CartContext.jsx";
+import UserProgressContext from "../store/UserProgressContext.jsx";
 
 export default function Header() {
   const cartContext = useContext(CartContext);
+  const userProgressContext = useContext(UserProgressContext);
+
   const totalItems = cartContext.items.reduce((acc, item) => {
     return acc + item.amount;
   }, 0);
+
+  const handleOpenCart = () => {
+    userProgressContext.showCart();
+  };
+
   return (
     <header id="main-header">
       <div id="title">
@@ -18,7 +26,9 @@ export default function Header() {
         <h1>My Food Order App</h1>
       </div>
       <nav>
-        <Button textOnly> My Cart ({totalItems}) </Button>
+        <Button textOnly onClick={handleOpenCart}>
+          My Cart ({totalItems})
+        </Button>
       </nav>
     </header>
   );
